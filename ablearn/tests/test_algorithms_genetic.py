a95efe249185b0996e6eca2bca8979cb0432b28e
Vince Knight <vincent.knight@gmail.com>
2015-06-22 12:22:55 +0100

    Deleting broken tests in genetic algorithm part

--- ablearn/tests/test_algorithms_genetic.py
+++ ablearn/tests/test_algorithms_genetic.py
@@ -1,17 +1,1 @@
-import unittest
-from ablearn.Abm import kill_one_agent_with_given_utility
-
-class AgentTesting(unittest.TestCase):
-	
-	def test_killing_agents_correct(self):
-		a = agents[2].utility
-		u = utility
-		if a == u:
-			self.assertNotIn(agents[2], agents)
-	
-		
-
-	def test_killing_agent_returns_error_if_agents_not_numbers(self):	
-		self.kill = kill_one_agent_with_given_utility()
-		self.assertRaises(ValueError, self.kill, 'ten', 0)
 
